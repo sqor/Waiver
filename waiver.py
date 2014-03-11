c9521834ccb0ced1a8316e541605e0b07f197f12
purnell-sqor <purnell@sqor.com>
2014-03-11 10:53:23 -0700

    moved files around and handle missing file issue

--- waiver.py
+++ waiver.py
@@ -1,13 +1,24 @@
-import requests
+#!/usr/bin/python
+'''
+
+'''
+
 import os
-from HTMLParser import HTMLParser
-from htmlentitydefs import name2codepoint
+import sys
 import argparse
+import requests
+from HTMLParser import HTMLParser
 
 class MyHTMLParser(HTMLParser):
-    scriptUrls = []
-    htmlData = []
-    handle_script_tag = False
+    '''
+    Parse given html and handle the script tags.
+    '''
+    def __init__(self, *args, **kwargs):
+        HTMLParser.__init__(self, *args, **kwargs)
+        self.scriptUrls = []
+        #@TODO: consider converting data to text here
+        self.htmlData = []
+        self.handle_script_tag = False
 
     def handle_starttag(self, tag, attrs):
         self.handle_script_tag = False
@@ -29,26 +40,51 @@ class MyHTMLParser(HTMLParser):
         self.handle_script_tag = False
 
     def handle_data(self, data):
-
         if not self.handle_script_tag:
             self.htmlData.append({"data": data} )
 
-def getargs():
+    def handle_comment(self, data):
+        self.htmlData.append({"data": data} )
+    '''
+    def handle_entityref(self, name):
+        c = unichr(name2codepoint[name])
+        print "Named ent:", c
+    def handle_charref(self, name):
+        if name.startswith('x'):
+            c = unichr(int(name[1:], 16))
+        else:
+            c = unichr(int(name))
+        print "Num ent  :", c
+    def handle_decl(self, data):
+        print "Decl     :", data
+    '''
+
+def parse_args(input_args):
+    '''
+    Parse the command line arguments and return an object containing the
+    arguments.
+
+    PARAM input_args list
+        List of input arguments to parse.
+    '''
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--html_dir',
                         help='directory to find the html file.')
     parser.add_argument('--html_filename',
                         help='html file to parse.')
 
-    return parser.parse_args()
+    return parser.parse_args(input_args)
 
 def main():
-    args = getargs()
+    '''
+    
+    '''
+    # get the commandline arguments
+    args = parse_args(sys.argv[1:])
 
     input_html_dir = args.html_dir
     input_html_filename = args.html_filename
     html_filepath = os.path.join(input_html_dir, input_html_filename)
-    print html_filepath
 
     # Get the html file:
     parser = MyHTMLParser()
@@ -80,7 +116,7 @@ def main():
         if data.has_key('data'):
             new_html_contents.append(data['data'])
 
-    print ''.join(new_html_contents)
+    #print ''.join(new_html_contents)
 
     #@TODO: date/timestamp
     url_file_contents = ""
@@ -89,13 +125,15 @@ def main():
             r = requests.get(url)
             url_file_contents += r.content
         else:
-            print html_filepath
             url_fullpath = os.path.join(input_html_dir, url)
-            with open(url_fullpath, 'r') as fh:
-                url_file_contents += fh.read()
+            if os.path.isfile(url_fullpath):
+                with open(url_fullpath, 'r') as fh:
+                    url_file_contents += fh.read()
+            else:
+                print "Couldn't find file: %s" % (url_fullpath)
         url_file_contents += '\n'
 
-    with open(os.path.join(args.html_dir, 'main001.js'), 'w') as fh:
+    with open(os.path.join(input_html_dir, 'main001.js'), 'w') as fh:
         fh.write(url_file_contents)
 
 if __name__ == '__main__':
